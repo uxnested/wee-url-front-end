0e9d0e38cca05e58fc49e4d8de8381708aca6527
Mehedi Hasan Sabbir <mohammadmehedihasansabbir@gmail.com>
2023-03-11 22:08:37 +0600

    added folder structure & page layout

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+// import "@fontsource/karla";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  const [showChild, setShowChild] = useState(false);
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Head>
+        <title>Wee Url </title>
+        <link rel="shortcut icon" href={"/logo.png"} type="image/x-icon" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
